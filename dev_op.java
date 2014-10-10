31e9514c787727c3cb203d841830f886dfa0c217
KristianPopov <kristian@popovbg.eu>
2014-10-10 18:51:34 +0300

    Create dev_op.java

---
+++ dev_op.java
@@ -0,0 +1,31 @@
+
+public class DevideOperation  implements Operation {
+	private double a;
+	private double b;
+	
+	public double getA() {
+		return a;
+	}
+	public void setA(double a) {
+		this.a = a;
+	}
+	
+	public double getB() {
+		return b;
+	}
+	public void setB(double b) {
+		this.b = b;
+	}
+	
+	public DevideOperation(double new_a, double new_b) {
+		setA(new_a);
+		setB(new_b);
+	}
+	
+	public double execute(){
+		if(b!=0){
+			return a/b;
+		}
+		else return 0;
+	}
+}
